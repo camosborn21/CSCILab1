9a059faa57c68f29c811bd55af9d88dfbd9bc1d0
Cameron Osborn <camosborn21@msn.com>
2017-09-06 19:38:19 -0700

    Final Commit on 9/6

--- CSCILab1/CSCILab1.cpp
+++ CSCILab1/CSCILab1.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 using namespace std;
 
+//[9/6/2017 18:34] Cameron Osborn: Testing to see how class scoped variables work in C++. Update: Apparently they work the same as C#.
+vector<string> myVect;
 
 //[9/6/2017 13:50] Cameron Osborn:  Display all elements of string typed vector parameter. 
 void IterateThroughStringVector(vector<string> vec)
@@ -18,7 +20,10 @@ void IterateThroughStringVector(vector<string> vec)
 
 }
 
-int main()
+
+
+//[9/6/2017 18:29] Cameron Osborn: Refactoring tasks to individual procedures for unit testability
+void Task1()
 {
 	//Task #1: Manually store 3 lines of source code in a vector of strings
 	//	Declare a vector of strings and name it myVect.Use the push_back method of the vector class to store the following three strings into myVect :
@@ -28,12 +33,15 @@ int main()
 	cout << "Task 1" << endl;
 	cout << "No Output" << endl;
 
-	vector<string> myVect;
 	myVect.push_back("radius=2;");
 	myVect.push_back("area=3.14*radius*radius;");
 	myVect.push_back("display area;");
+	
+}
 
 
+void Task2()
+{
 	//Task #2: Access the elements by their indices
 	//	Write a loop to display all the elements myVect[i] in myVect one by one.
 	cout << endl << endl << "Task 2" << endl;
@@ -41,18 +49,21 @@ int main()
 	{
 		cout << myVect[i] << endl;
 	}
+}
 
-
+void Task3()
+{
 	//Task #3: Access the elements using an iterator
 	//	Write a loop using a vector<string>::iterator iterator to iterate through the vector myVect and to display all the elements in myVect one by one.
 	cout << endl << endl << "Task 3" << endl;
 
 	//[9/6/2017 13:47] Cameron Osborn: Due to reuse of this procedure I refactored it to its own function above
 	IterateThroughStringVector(myVect);
+}
 
-
+void Task4()
+{
 	//Task #4: Use an iterator to delete some content
-
 	//	Call the erase method to delete the second element in myVect. (i.e. delete myVect[1] from the vector)
 	//	Check the new contents : Write a loop using a vector<string>::iterator iterator to iterate through the vector myVect and to display all the elements in myVect one by one.
 	cout << endl << endl << "Task 4" << endl;
@@ -61,7 +72,10 @@ int main()
 	myVect.erase(itDel);
 	IterateThroughStringVector(myVect);
 
+}
 
+void Task5()
+{
 	//Task #5: Use an iterator to insert new content
 	//	Call the insert method of the vector class to insert a new string "display radius;" right before the element currently holding "display area;" in myVect.
 	//	Check the new contents : Write a loop using a vector<string>::iterator iterator to iterate through the vector myVect and to display all the elements in myVect one by one.
@@ -70,16 +84,21 @@ int main()
 	vector<string>::iterator itIns = myVect.end() - 1;
 	myVect.insert(itIns, "display radius;");
 	IterateThroughStringVector(myVect);
+}
 
 
-
+void Task6()
+{
 	//Task #6: Clear the contents of the entire vector
 	//	Call the clear method to clear the vector myVect into an empty vetor.
 	//	Check the new contents : Write a loop using a vector<string>::iterator iterator to iterate through the vector myVect and to display all the elements in myVect one by one.
 	cout << endl << endl << "Task 6" << endl;
 	myVect.clear();
 	IterateThroughStringVector(myVect);
+}
 
+void Task7()
+{
 	//Task #7: Use getline to read an arbitrary number of lines of source code into a vector of strings
 	//	Declare a string object s and then write a loop which on each iteration(i) calls getline(cin, s) to read in a line into s, (ii) if the contents of s is neither an empty string nor the special string ".", calls the push_back method to store it in the end of the vector myVect, and (iii)the loop will terminate if the contents of s is special string "."
 	//	Check the new contents : Write a loop using a vector<string>::iterator iterator to iterate through the vector myVect and to display all the elements in myVect one by one.
@@ -89,7 +108,7 @@ int main()
 	while (s != ".")
 	{
 		getline(cin, s);
-		if (s != "." & s!="")
+		if (s != "." & s != "")
 		{
 			myVect.push_back(s);
 		}
@@ -97,6 +116,24 @@ int main()
 
 	cout << endl << endl << "Task 7 Output:" << endl;
 	IterateThroughStringVector(myVect);
+}
+int main()
+{
+	//[9/6/2017 18:30] Cameron Osborn: Calls to individual task execution
+	Task1();
+
+	Task2();
+
+	Task3();
+
+	Task4();
+
+	Task5();
+
+	Task6();
+
+	Task7();
+
 	return 0;
 }
 
